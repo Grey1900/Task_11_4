4ff11339c843e69cdf97e741032c561cd121028f
TimkyWimky <36457305+Grey1900@users.noreply.github.com>
2023-02-01 14:53:57 +0400

    commit upgrade checkInput func, add row, columns, diagonals check

--- task_11_4.cpp
+++ task_11_4.cpp
@@ -8,77 +8,65 @@
 #include <iostream>
 #include <string>
 
-bool checkInput(std::string upper, std::string middle, std::string lower) {
+const int N = 3;
+char board[N][N];
+bool win = false;
+int x_count = 0, o_count = 0;
 
-    for (int i = 0; i < 3; i++) {
-        if (upper[i] == 'X' && middle[i] == 'X' && lower[i] == 'X') return false;
-        if (upper[i] == 'O' && middle[i] == 'O' && lower[i] == 'O') return false;
-    }
+bool checkInput(char board[N][N]) {
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (upper[j] == 'X' && middle[j] == 'X' && lower[j] == 'X') return false;
-            if (upper[j] == 'O' && middle[j] == 'O' && lower[j] == 'O') return false;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (board[i][j] == 'X') x_count++;
+            else if (board[i][j] == 'O') o_count++;
+            else if (board[i][j] != '.') return false;
         }
     }
-
-    for (int i = 0; i < 3; i++) {
-        if (upper[i] == 'X' && middle[1] == 'X' && lower[2 - i] == 'X') return false;
-        if (upper[i] == 'O' && middle[1] == 'O' && lower[2 - i] == 'O') return false;
-    }
-
+    if (x_count - o_count < 0 || x_count - o_count > 1) return false;
     return true;
 }
 
-
-std::string checkWin(std::string upper, std::string middle, std::string lower)
-{
-    char board[3][3] = {
-        {upper[0], upper[1], upper[2]},
-        {middle[0], middle[1], middle[2]},
-        {lower[0], lower[1], lower[2]}
-    };
-
-    for(int i = 0; i < 3; i++)
-    {
-        if(board[i][0] == board[i][1] && board[i][1] == board[i][2])
-        {
-            if(board[i][0] == 'X')
-                return "Petya won";
-            if(board[i][0] == 'O')
-                return "Vasya won";
-        }
+bool checkRows(char board[N][N]) {
+    for (int i = 0; i < N; i++) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != '.') return true;
     }
-
-    if(board[0][0] == board[1][1] && board[1][1] == board[2][0])
-    {
-        if(board[0][2] == 'X')
-            return "Petya won";
-        if(board[0][2] == 'O')
-            return "Vasya won";
-    }
-
-    for(int i = 0; i < 3; i++)
-    {
-        for(int j = 0; j < 3; j++)
-        {
-            if(board[i][j] == '.')
-                return "Nobody";
-        }
-    }
-    return "Nobody";
+    return false;
 }
 
-int main()
-{
-    std::string upper, middle, lower;
-    std::cin >> upper >> middle >> lower;
+bool checkColumns(char board[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != '.') return true;
+        }
+    }
+    return false;
+}
 
-    if(!checkInput(upper, middle, lower))
-    {
-        std::cout << "Incorrect";
+bool checkDiagonals(char board[N][N]) {
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '.') return true;
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != '.') return true;
+    return false;
+}
+
+int main() {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            std::cin >> board[i][j];
+        }
+    }
+    if (!checkInput(board)) {
+        std::cout << "Incorrect" << std::endl;
         return 0;
     }
-    std::cout << checkWin(upper, middle, lower);
+    if (checkRows(board) || checkColumns(board) || checkDiagonals(board)) {
+        win = true;
+    }
+    if (win) {
+        if (x_count > o_count) std::cout << "Petya won" << std::endl;
+        else std::cout << "Vasya won" << std::endl;
+    } else {
+        std::cout << "Nobody" << std::endl;
+    }
     return 0;
 }
+
