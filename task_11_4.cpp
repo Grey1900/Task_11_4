88f2f54f66cd2aa9f8c8b839461e87692eb7e1a5
TimkyWimky <36457305+Grey1900@users.noreply.github.com>
2023-02-01 13:11:19 +0400

    commit upgrade checkInput func

--- task_11_4.cpp
+++ task_11_4.cpp
@@ -8,21 +8,30 @@
 #include <iostream>
 #include <string>
 
-bool checkInput(std::string upper, std::string middle, std::string lower) {
+bool checkInput(string upper, string middle, string lower) {
+
     for (int i = 0; i < 3; i++) {
-        if (upper[i] != 'X' && upper[i] != 'O' && upper[i] != '.') {
-            return false;
-        }
-        if (middle[i] != 'X' && middle[i] != 'O' && middle[i] != '.') {
-            return false;
-        }
-        if (lower[i] != 'X' && lower[i] != 'O' && lower[i] != '.') {
-            return false;
-        }
-        return true;
+        if (upper[i] == 'X' && middle[i] == 'X' && lower[i] == 'X') return false;
+        if (upper[i] == 'O' && middle[i] == 'O' && lower[i] == 'O') return false;
     }
-    return 0;
+
+    for (int i = 0; i < 3; i++) {
+        if (upper[0] == 'X' && middle[0] == 'X' && lower[0] == 'X') return false;
+        if (upper[1] == 'X' && middle[1] == 'X' && lower[1] == 'X') return false;
+        if (upper[2] == 'X' && middle[2] == 'X' && lower[2] == 'X') return false;
+        if (upper[0] == 'O' && middle[0] == 'O' && lower[0] == 'O') return false;
+        if (upper[1] == 'O' && middle[1] == 'O' && lower[1] == 'O') return false;
+        if (upper[2] == 'O' && middle[2] == 'O' && lower[2] == 'O') return false;
+    }
+
+    if (upper[0] == 'X' && middle[1] == 'X' && lower[2] == 'X') return false;
+    if (upper[2] == 'X' && middle[1] == 'X' && lower[0] == 'X') return false;
+    if (upper[0] == 'O' && middle[1] == 'O' && lower[2] == 'O') return false;
+    if (upper[2] == 'O' && middle[1] == 'O' && lower[0] == 'O') return false;
+
+    return true;
 }
+
 
 std::string checkWin(std::string upper, std::string middle, std::string lower)
 {
